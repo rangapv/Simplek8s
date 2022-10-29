510d96bb15bf0a5d86b718fd8027f5abd4f6a40f
rangapv <rangapv@yahoo.com>
2022-10-29 12:59:58 +0000

    Added CCM install scripts for both Master and Worker Nodes

---
+++ simpleccm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -E
+#Run this script with ./simpleccm.sh Access_ID Secret_id
+#Assumption the ubuntu 22.04 ec2 instance has been populated with user data and Role k8srole is attached
+
+# setup the master node with Tag, Nodename format etc
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metasetup.sh)  >>/dev/null 2>&1
+
+# run containerdD runtime
+source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)  >>/dev/null 2>&1
+
+# TO install latest version of k8s for the Master/Control Node
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sinit-adm.sh)  >>/dev/null 2>&1
+
+#To install flannel and Dashboard
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_adm/k8sfladash.sh)  >>/dev/null 2>&1
+
+#To Install the cloud-provider flags on the components & kubelet
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/ccmstyle.sh)  >>/dev/null 2>&1
+
+#To install the RBAC for CCM
+`kubectl apply -f https://raw.githubusercontent.com/rangapv/Kube-Manifests/master/CCM/ccm-rbac-outree.yaml`
+
+#To Install the Statefule set ebs-csi on aws ec2 
+#source <(curl -s https://raw.githubusercontent.com/rangapv/Kube-Manifests/master/StatefulSet/aws/ebs-csi/install-csi.sh)  >>/dev/null 2>&1
---
+++ simplenodeccm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -E
+#Run this script with ./simplenodeccm.sh Access_ID Secret_id
+#Assumption the ubuntu 22.04 ec2 instance has been populated with user data and Role k8srole is attached
+#SFTP the kubeconfig file from the master node to this particualr worker node(ec2 instance)
+#Keep the JOIN comand handy....
+
+# setup the node with Tag, Nodename format etc
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/metasetup.sh)  >>/dev/null 2>&1
+
+# run containerdD runtime
+source <(curl -s https://raw.githubusercontent.com/rangapv/runtimes/main/runcontainerD.sh)  >>/dev/null 2>&1
+
+# TO install latest version of k8s for the Worker Node
+source <(curl -s https://raw.githubusercontent.com/rangapv/k8s/master/kube_node/k8snodeinstall.sh)  >>/dev/null 2>&1
+
+# TO setup config file transferred from Master node 
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/nodeconfig.sh)  >>/dev/null 2>&1
+
+#To CCM only configure the cloud-provider flags on the kubelet
+source <(curl -s https://raw.githubusercontent.com/rangapv/metascript/main/ccmstyle.sh)  >>/dev/null 2>&1
